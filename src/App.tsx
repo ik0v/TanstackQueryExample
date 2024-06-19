c74a4db1b833599170c1160272f29d5602a2d033
ik0v <ivan.kovals@hotmail.com>
2024-06-19 21:34:12 +0200

    first steps - todos fetched

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { useQuery } from "@tanstack/react-query";
+
+function App() {
+  const startTime = window.performance.now();
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["todo"],
+    queryFn: () =>
+      fetch("https://jsonplaceholder.typicode.com/todos").then((res) =>
+        res.json()
+      ),
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error loading data: {error.message}</p>;
+
+  const endTime = window.performance.now();
+  console.log("Load time:" + (endTime - startTime) + " ms");
+
+  return (
+    <div className="App">
+      {data.map((todo: any) => (
+        <div key={todo.id}>
+          <h5>ID: {todo.id}</h5>
+          <p>Title: {todo.title}</p>
+          <hr />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
